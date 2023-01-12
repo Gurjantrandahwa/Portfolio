b44857e801776622460c2d5f44e68b8ebe29939d
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-01-12 17:19:03 +0530

    Portfolio implemented

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./Contact.css";
-import {EnvelopeIcon, PhoneIcon} from "@heroicons/react/24/solid";
-import {MapPinIcon} from "@heroicons/react/20/solid";
 import {Button, TextField} from "@mui/material";
 
 export default function Contact() {
